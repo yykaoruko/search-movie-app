1bac60588257bba5a1db03c330f33c3ea10b99a8
染矢馨子 <>
2019-11-17 21:54:38 +0900

    feat: しゃきょー

---
+++ src/components/App/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "../Header/Header";
+import Movie from "../Movie/Movie";
+import Search from "../Search/Search";
+
+const MOVIE_API_URL = "https://www.omdbapi.com/?s=man&apikey=fcb61aea";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setMovies(jsonResponse.Search);
+        setLoading(false);
+      });
+  }, []);
+
+  const search = searchValue => {
+    setLoading(true);
+    setErrorMessage(null);
+
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=fcb61aea`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response === "True") {
+          setMovies(jsonResponse.Search);
+          setLoading(false);
+        } else {
+          setErrorMessage(jsonResponse.Error);
+          setLoading(false);
+        }
+      });
+  };
+
+  return (
+    <div className="App">
+      <Header text="Search!" />
+      <Search search={search} />
+      <div className="movies">
+        {loading && !errorMessage ? (
+          <span>loading...</span>
+        ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map((movie, index) => (
+            <Movie key={`${index}-${movie.Title}`} movie={movie} />
+          ))
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
